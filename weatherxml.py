10292e433b37d3c664bb78e5b6ec2edcfd5a437f
JoseluisParejo <joseluisparejofernandez@gmail.com>
2014-03-05 14:21:59 +0100

    añadido
    añadir mas codigo basico

--- weatherxml.py
+++ weatherxml.py
@@ -2,3 +2,15 @@
 import requests
 import json
 from lxml import etree
+from jinja2 import Template
+
+ciudades = """
+"Almeria",
+"Cadiz",
+"Cordoba",
+"Granada",
+"Huelva",
+"Jaen",
+"Malaga",
+"Sevilla" """
+
